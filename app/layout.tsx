f4f9af3bc843d93bcbd8efee3a7a9b15c81f0a82
omykkru089 <sasha20031218@gmail.com>
2025-01-17 14:11:45 +0100

    v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './ui/global.css'
+import { Footer } from './ui/footer';
 
 export default function RootLayout({
   children,
@@ -9,6 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -7,10 +7,22 @@ export function Header() {
         <Image src="/GameShop.png" alt="icono de la web" width={120} height={120}></Image>
             <div class="menu-plataformas">
                 <nav >
-                    <Link href="#">PC</Link>
-                    <Link href="#">Xbox</Link>
-                    <Link href="#">PlayStation</Link>
-                    <Link href="#">Nintendo Switch</Link>
+                    <Link href="#">
+                    <Image src="/iconPC.png" alt="icono de pc" width={20} height={20}></Image>
+                    PC
+                    </Link>
+                    <Link href="#">
+                    <Image src="/iconXbox.png" alt="icono de xbox" width={20} height={20}></Image>
+                    Xbox
+                    </Link>
+                    <Link href="#">
+                    <Image src="/iconPS.png" alt="icono de xbox" width={20} height={20}></Image>
+                    PlayStation
+                    </Link>
+                    <Link href="#">
+                    <Image src="/iconNS.png" alt="icono de xbox" width={20} height={20}></Image>
+                    Nintendo 
+                    </Link>
                 </nav>
             </div>
             <div class="menu-auth">
